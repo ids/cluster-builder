4e05718593bec9f8a65b0efe57e3a79b24530aa1
Sean Hignett <sean@intelligent-design.ca>
2017-12-31 08:12:16 -0700

    fixes for centos-ucp, photon re-org to packer centric

--- node-packer/bin/photon-setup.sh
+++ node-packer/bin/photon-setup.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
-DOCKER_VERSION=17.09.0-ce
+DOCKER_VERSION=17.12.0-ce
 
-echo "***"
-echo "*** Setting up authorized keys..."
-echo "***"
+#echo "***"
+#echo "*** Setting up authorized keys..."
+#echo "***"
 
-if [ ! -d /root/.ssh ]; then 
-  mkdir /root/.ssh
-  chmod 700 /root/.ssh
-fi 
-cp /tmp/authorized_keys /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
+#if [ ! -d /root/.ssh ]; then 
+#  mkdir /root/.ssh
+#  chmod 700 /root/.ssh
+#fi 
+#cp /tmp/authorized_keys /root/.ssh/authorized_keys
+#chmod 600 /root/.ssh/authorized_keys
 
 echo "***"
 echo "*** Installing:"
 echo "***   - VMware Tools"
 echo "***   - NFS Support"
+echo "***   - PIP & Ansible"
 echo "***   - Upgrade Docker to ${DOCKER_VERSION}"
 echo "***"
 
-tdnf -y install open-vm-tools nfs-utils tar gzip curl ntp python2
+tdnf -y install open-vm-tools nfs-utils tar gzip curl ntp python2 python-pip
 
 echo "*** Downloading Docker ${DOCKER_VERSION}"
 curl -fsSLO https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz 
@@ -30,26 +31,16 @@ echo "*** Configuring Docker ${DOCKER_VERSION}"
 echo '[Unit]
 Description=Docker Application Container Engine
 Documentation=https://docs.docker.com
-After=network-online.target
+After=network-online.target firewalld.service
 Wants=network-online.target
 
 [Service]
 Type=notify
-# the default is not to use systemd for cgroups because the delegate issues still
-# exists and systemd currently does not support the cgroup feature set required
-# for containers run by docker
-EnvironmentFile=-/etc/default/docker
-ExecStart=/usr/bin/dockerd $DOCKER_OPTS
+ExecStart=/usr/bin/dockerd 
 ExecReload=/bin/kill -s HUP $MAINPID
-# Having non-zero Limit*s causes performance problems due to accounting overhead
-# in the kernel. We recommend using cgroups to do container-local accounting.
 LimitNOFILE=infinity
 LimitNPROC=infinity
 LimitCORE=infinity
-# Uncomment TasksMax if your systemd version supports it.
-# Only systemd 226 and above support this version.
-#TasksMax=infinity
-TimeoutStartSec=0
 # set delegate yes so that systemd does not reset the cgroups of docker containers
 Delegate=yes
 # kill only the docker process, not all processes in the cgroup
@@ -69,3 +60,14 @@ systemctl start docker
 
 echo "*** Clearing the DHCP uuid used by Photon"
 echo -n > /etc/machine-id
+
+echo '*** Upgrading pip'
+pip install --upgrade pip
+
+# Avoid bug in default python cryptography library
+# [WARNING]: Optional dependency 'cryptography' raised an exception, falling back to 'Crypto'
+echo '*** Upgrading python cryptography library'
+pip install --upgrade cryptography
+
+echo '*** Installing Ansible'
+pip install ansible==2.4.1
