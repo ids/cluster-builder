69d43fe284495565ac3e7f0d9e07f9886a589971
Sean Hignett <sean@intelligent-design.ca>
2018-07-02 04:07:50 -0600

    added nginx ingress controller template and some additional k8s manifests

---
+++ xtras/k8s/nginx-init.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/baremetal/service-nodeport.yaml
