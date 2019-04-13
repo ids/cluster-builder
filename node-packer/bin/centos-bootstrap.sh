3ebd1059339cd79b2838799a36f468269908c824
Sean Hignett <sean@intelligent-design.ca>
2019-04-13 12:38:33 -0600

    pruned python deps down to ansible install

--- node-packer/bin/centos-bootstrap.sh
+++ node-packer/bin/centos-bootstrap.sh
@@ -14,10 +14,6 @@ yum install -y yum-utils
 
 # Add the EPEL repository, and install Ansible.
 echo '>>> Adding EPEL yum repo'
-#yum-config-manager --add-repo=https://dl.fedoraproject.org/pub/epel/7/x86_64/
-#curl --fail --location --silent --show-error --verbose -o /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7 https://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7
-#rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
-
 yum install -y epel-release
 yum repolist
 
@@ -29,21 +25,8 @@ yum clean all
 
 export PATH=/usr/local/bin:$PATH
 
-echo '>>> Installing pip3 (and dependencies)'
-yum install -y python36 python36-devel python36-setuptools python36-pip
-yum install -y libffi-devel openssl-devel gcc redhat-rpm-config
-
-python3 --version
-which pip3
-pip3 --version
-
-pip3 install --upgrade pip
-pip --version
-
-# Avoid bug in default python cryptography library
-# [WARNING]: Optional dependency 'cryptography' raised an exception, falling back to 'Crypto'
-echo '>>> Upgrading python cryptography library'
-pip install --upgrade cryptography
+echo '>>> Installing general dependencies'
+yum install -y libffi-devel openssl-devel gcc redhat-rpm-config open-vm-tools
 
 echo '>>> Installing Ansible'
-pip install ansible==2.7.8
+yum install -y ansible
--- node-packer/bin/fedora-bootstrap.sh
+++ node-packer/bin/fedora-bootstrap.sh
@@ -6,21 +6,8 @@ set -o pipefail
 
 export PATH=/usr/local/bin:$PATH
 
-echo '>>> Installing pip3 (and dependencies)'
-dnf install -y python3 python3-devel python3-pip
+echo '>>> Installing general dependencies'
 dnf install -y libffi-devel openssl-devel gcc redhat-rpm-config open-vm-tools
 
-python3 --version
-which pip3
-pip3 --version
-
-pip3 install --upgrade pip
-pip --version
-
-# Avoid bug in default python cryptography library
-# [WARNING]: Optional dependency 'cryptography' raised an exception, falling back to 'Crypto'
-echo '>>> Upgrading python cryptography library'
-pip install --upgrade cryptography
-
 echo '>>> Installing Ansible'
-pip install ansible==2.7.8
+dnf install -y ansible
