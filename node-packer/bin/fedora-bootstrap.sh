303c70eccdc18673454ab92a84af97150920a6db
Sean Hignett <sean@intelligent-design.ca>
2019-04-13 12:11:14 -0600

    tweaks to fedora python

--- node-packer/bin/fedora-bootstrap.sh
+++ node-packer/bin/fedora-bootstrap.sh
@@ -7,9 +7,8 @@ set -o pipefail
 export PATH=/usr/local/bin:$PATH
 
 echo '>>> Installing pip3 (and dependencies)'
-dnf install -y python36
-dnf install -y python3-pip
-dnf install -y python3-devel.x86_64 libffi-devel openssl-devel gcc redhat-rpm-config open-vm-tools
+dnf install -y python36 python36-devel python36-setuptools python36-pip
+dnf install -y libffi-devel openssl-devel gcc redhat-rpm-config open-vm-tools
 
 python3 --version
 which pip3
