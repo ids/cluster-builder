9c9d94147c92d48c1f67cd3efa9f683b23f03724
Sean Hignett <sean@intelligent-design.ca>
2019-07-13 13:31:20 -0600

    begin switch to python3 ansible in fedora

--- node-packer/bin/fedora-bootstrap.sh
+++ node-packer/bin/fedora-bootstrap.sh
@@ -5,15 +5,15 @@ set -o nounset
 set -o pipefail
 
 echo '>>> Installing pip (and dependencies)'
-dnf install -y python-devel libffi-devel openssl-devel gcc python-pip redhat-rpm-config open-vm-tools
+dnf install -y python3-devel libffi-devel openssl-devel gcc redhat-rpm-config open-vm-tools
 
-echo '>>> Upgrading pip'
-pip install --upgrade pip
+echo '>>> Upgrading pip3'
+pip3 install --upgrade pip3
 
 # Avoid bug in default python cryptography library
 # [WARNING]: Optional dependency 'cryptography' raised an exception, falling back to 'Crypto'
 echo '>>> Upgrading python cryptography library'
-pip install --upgrade cryptography
+pip3 install --upgrade cryptography
 
 echo '>>> Installing Ansible'
-pip install ansible==2.7.8
+pip3 install ansible==2.8.2
