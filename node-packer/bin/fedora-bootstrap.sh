2219500912014817a959294907c542019b70df1b
Sean Hignett <sean@intelligent-design.ca>
2019-12-24 18:58:53 -0700

    fix for fedora

--- node-packer/bin/fedora-bootstrap.sh
+++ node-packer/bin/fedora-bootstrap.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 echo '>>> Installing base dependencies'
-dnf install -y python3 python3-devel libffi-devel openssl-devel gcc redhat-rpm-config open-vm-tools
+dnf install -y python3 python3-pip python3-devel libffi-devel openssl-devel gcc redhat-rpm-config open-vm-tools
 
 echo '>>> Installing ansible'
 pip3 install ansible
