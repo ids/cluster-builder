14903b0cbc9dbe2a85a9d218ea1a6a7e2335e356
Sean Hignett <sean@intelligent-design.ca>
2020-04-29 17:25:54 -0600

    fixes in support of 20.04 re-release, upgrade to Fedora 31

--- node-packer/bin/fedora-bootstrap.sh
+++ node-packer/bin/fedora-bootstrap.sh
@@ -5,8 +5,27 @@ set -o nounset
 set -o pipefail
 
 echo '>>> Installing base dependencies'
+dnf makecache
 dnf install -y python3 python3-pip python3-devel libffi-devel openssl-devel gcc redhat-rpm-config open-vm-tools
 
 echo '>>> Installing ansible'
 pip3 install ansible
 
+#echo '>>> Installing docker-ce'
+#sudo dnf -y install dnf-plugins-core
+
+#sudo tee /etc/yum.repos.d/docker-ce.repo<<EOF
+#[docker-ce-stable]
+#name=Docker CE Stable - \$basearch
+#baseurl=https://download.docker.com/linux/fedora/31/\$basearch/stable
+#enabled=1
+#gpgcheck=1
+#gpgkey=https://download.docker.com/linux/fedora/gpg
+#EOF
+
+#sudo dnf makecache
+#sudo dnf -y install docker-ce docker-ce-cli containerd.io
+
+echo '>>> Switch to CGroup v1'
+dnf install -y grubby
+grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
